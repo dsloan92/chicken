4116d74320481db2c839bd9192e5c32c0edaa336
Dallas Sloan <dsloan4@mail.greenriver.edu>
2020-01-15 14:43:39 -0500

    Added views directory

--- index.php
+++ index.php
@@ -5,7 +5,22 @@
  *  index.php
  * Chicken in class follow along
  */
+
+//require auto-load file
+require ("vendor/autoload.php");
 //Turn on error reporting -- this is critical!
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Create an instance of the base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3->route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/all-about-chickens.html');
+    echo "<h1>Hello World</h1>";
+});
+
+//Run fat free
+$f3->run();
