eb453466d26fb7e24b33086bba5b3fc620cd9cb5
Dallas Sloan <dsloan4@mail.greenriver.edu>
2020-01-15 14:14:42 -0500

    Added an index page with comment headers

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/** Name: Dallas Sloan
+ *  Date Jan 6, 2019
+ *  index.php
+ * Chicken in class follow along
+ */
+//Turn on error reporting -- this is critical!
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
